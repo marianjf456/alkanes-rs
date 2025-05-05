7f4ae9559f5bc1b57e36c678961f890b126a5f55
Hude Intres <hudeintres@gmail.com>
2025-05-05 19:00:34 +0000

    edict revert test

--- src/tests/edict_then_message.rs
+++ src/tests/edict_then_message.rs
@@ -198,3 +198,63 @@ fn test_edict_message_same_protostone() -> Result<()> {
 
     Ok(())
 }
+
+#[wasm_bindgen_test]
+fn test_edict_message_same_protostone_revert() -> Result<()> {
+    clear();
+    let block_height = 840_000;
+
+    // Create a cellpack to call the process_numbers method (opcode 11)
+    let arb_mint_cellpack = Cellpack {
+        target: AlkaneId { block: 1, tx: 0 },
+        inputs: vec![30, 2, 1, 1],
+    };
+
+    // Initialize the contract and execute the cellpacks
+    let mut test_block = alkane_helpers::init_with_multiple_cellpacks_with_tx(
+        [alkanes_std_test_build::get_bytes()].into(),
+        [arb_mint_cellpack].into(),
+    );
+
+    index_block(&test_block, block_height)?;
+
+    let mut test_block2 = create_block_with_coinbase_tx(block_height + 1);
+
+    let input_script = ScriptBuf::new();
+    let txin1 = TxIn {
+        previous_output: OutPoint {
+            txid: test_block.txdata[test_block.txdata.len() - 1].compute_txid(),
+            vout: 0,
+        },
+        script_sig: input_script.clone(),
+        sequence: Sequence::MAX,
+        witness: Witness::new(),
+    };
+
+    test_block2.txdata.push(
+        alkane_helpers::create_multiple_cellpack_with_witness_and_txins_edicts(
+            [Cellpack {
+                target: AlkaneId { block: 2, tx: 1 },
+                inputs: vec![100], //revert
+            }]
+            .into(),
+            vec![txin1],
+            false,
+            vec![ProtostoneEdict {
+                id: ProtoruneRuneId { block: 2, tx: 1 },
+                amount: 1,
+                output: 0,
+            }],
+        ),
+    );
+
+    index_block(&test_block2, block_height + 1)?;
+
+    let sheet = alkane_helpers::get_last_outpoint_sheet(&test_block2)?;
+
+    println!("Last sheet: {:?}", sheet);
+
+    assert_eq!(sheet.get_cached(&ProtoruneRuneId { block: 2, tx: 1 }), 1);
+
+    Ok(())
+}
